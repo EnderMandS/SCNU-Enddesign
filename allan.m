92361c18b2347420a7b01f126ed141d99bd58849
M <952025764@qq.com>
2023-03-06 16:43:38 +0800

    fft测试成功

--- allan.m
+++ allan.m
@@ -1,24 +1,30 @@
 sam_per_read = 500;
+sample_times = 4;
 Fs = 200;
 
-if isempty(ESP_obj)
+if ~exist("ESP_obj","var")
+    disp("Connecting to ESP32");
     ESP_obj = arduino("COM7",'ESP32-WROOM-DevKitC',"Libraries","I2C");
-    scanI2CBus(ESP_obj)
-    imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=Fs,SamplesPerRead=sam_per_read);
 end
-if isempty(imu_obj)==false
-    imu_obj.release
+clear imu_obj;
+scanI2CBus(ESP_obj)
+imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=Fs,SamplesPerRead=sam_per_read);
+disp("MPU Connected");
+
+accel_all = zeros(sam_per_read*sample_times,3);
+for i=0:sample_times-1
+    disp("Sampling");
+    [accel, ~, ~, ~] = imu_obj.read;
+    accel_all(i*sam_per_read+1:(i+1)*sam_per_read,:) = accel;
 end
-    imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=Fs,SamplesPerRead=sam_per_read);
+[avar,tau] = allanvar(accel_all,'octave',Fs);
+axAvar = avar(:,1);
+ayAvar = avar(:,2);
+azAvar = avar(:,3);
 
-[accel, ~, ~, ~] = imu_obj.read;
-[avar,tau] = allanvar(accel,Fs);
-ax_avar = avar(:,1);
-ay_avar = avar(:,2);
-az_avar = avar(:,3);
-
-tbl = table(tau,ax_avar,ay_avar,az_avar);
-loglog(tbl,"tbl",["ax_avar" "ay_avar" "az_avar"]);
+tbl = table(tau, axAvar, ayAvar, azAvar);
+head(tbl,3)
+loglog(tbl, "tau", ["axAvar" "ayAvar" "azAvar"]);
 grid on;
 legend;
 xlabel('\tau');
--- imu.m
+++ imu.m
@@ -1,8 +1,12 @@
+clear all
 % 参数定义
+global sam_per_read;
+global buffer_length;
 sam_per_read = 10;
-buffer_length = 1000;
+buffer_length = 400;
 fs = 200;
 f_range = (0:buffer_length-1)*(fs/buffer_length);
+fshift = (-buffer_length/2:buffer_length/2-1)*(fs/buffer_length);
 an_fft = true;
 
 % 创建数据缓冲区
@@ -11,18 +15,17 @@ ay_data = zeros(1,buffer_length);
 az_data = zeros(1,buffer_length);
 
 % 初始化硬件
-if isempty(ESP_obj)
+if ~exist("ESP_obj","var")
+    disp("Connecting to ESP32");
     ESP_obj = arduino("COM7",'ESP32-WROOM-DevKitC',"Libraries","I2C");
-    scanI2CBus(ESP_obj)
-    imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=fs,SamplesPerRead=sam_per_read);
 end
-if isempty(imu_obj)==false
-    imu_obj.release
-end
+clear imu_obj;
 imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=fs,SamplesPerRead=sam_per_read);
+disp("MPU Connected");
 
 % 定义动态绘图结构体
 if an_fft==false
+    disp("Running at normal mode");
     an_ax = animatedline('MaximumNumPoints',2000,'Color',"#0072BD");
     an_ay = animatedline('MaximumNumPoints',2000,'Color',"#D95319");
     an_az = animatedline('MaximumNumPoints',2000,'Color',"#EDB120");
@@ -30,12 +33,18 @@ if an_fft==false
     % an_gy = animatedline('MaximumNumPoints',2000,'Color',"#4DBEEE");
     % an_gz = animatedline('MaximumNumPoints',2000,'Color',"#A2142F");
 else
-    an_ax_fft = animatedline('MaximumNumPoints',f_range);
-    an_ay_fft = animatedline('MaximumNumPoints',f_range);
-    an_az_fft = animatedline('MaximumNumPoints',f_range);
+    disp("Running at fft mode");
+    an_ax_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#0072BD");
+    an_ay_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#D95319");
+    an_az_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#EDB120");
 end
+drawnow
+xlabel("f (Hz)");
+ylabel("|P(f)|");
+legend('ax','ay','az');
 
 data_cnt = 0;
+buffer_available = false;
 while 1
     [accel, gyro, timeStamps, ~] = imu_obj.read;
 
@@ -67,22 +76,24 @@ while 1
 %         addpoints(an_gz,duration_ms,gz_t);
     else
         if buffer_available
-            ax_fft = fft(ax_data);
-            ay_fft = fft(ay_data);
-            az_fft = fft(az_data);
+            ax_fft = fftshift(fft(ax_data));
+            ay_fft = fftshift(fft(ay_data));
+            az_fft = fftshift(fft(az_data));
             ax_power = abs(ax_fft).^2/buffer_length;
             ay_power = abs(ay_fft).^2/buffer_length;
             az_power = abs(az_fft).^2/buffer_length;
             %绘图
-            addpoints(an_ax_fft,f_range,ax_power);
-            addpoints(an_ay_fft,f_range,ay_power);
-            addpoints(an_az_fft,f_range,az_power);
+            addpoints(an_ax_fft,fshift,ax_power);
+            addpoints(an_ay_fft,fshift,ay_power);
+            addpoints(an_az_fft,fshift,az_power);
         end
     end
     drawnow
 end
 
 function data_source = dataShiftAppend(data_source,data_append)
+    global sam_per_read;
+    global buffer_length;
     data_source = circshift(data_source,-sam_per_read);
     data_source(1,buffer_length-sam_per_read+1:buffer_length) = data_append;
 end
