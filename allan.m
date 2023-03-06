5259ff5a9d3ab9d2d04b8a86cbd7a2bdfda01c1b
M <952025764@qq.com>
2023-03-06 20:07:58 +0800

    allan方差完成

--- allan.m
+++ allan.m
@@ -1,5 +1,5 @@
 sam_per_read = 500;
-sample_times = 4;
+sample_times = 5;
 Fs = 200;
 
 if ~exist("ESP_obj","var")
@@ -11,20 +11,35 @@ scanI2CBus(ESP_obj)
 imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAddress',0x68,SampleRate=Fs,SamplesPerRead=sam_per_read);
 disp("MPU Connected");
 
+fuse = complementaryFilter("SampleRate",200,"HasMagnetometer",false,"OrientationFormat","quaternion");
 accel_all = zeros(sam_per_read*sample_times,3);
+gyro_all = zeros(sam_per_read*sample_times,3);
 for i=0:sample_times-1
     disp("Sampling");
-    [accel, ~, ~, ~] = imu_obj.read;
+    [accel, gyro, ~, ~] = imu_obj.read;
     accel_all(i*sam_per_read+1:(i+1)*sam_per_read,:) = accel;
+    gyro_all(i*sam_per_read+1:(i+1)*sam_per_read,:) = gyro;
 end
+
+% [orientation_q,~] = fuse(accel_all,gyro_all);
+% eulerAngles = euler(orientation_q,'ZYX','point');
+% eulerAngles = rad2deg(eulerAngles);
+% yaw = eulerAngles(:,1);
+% pitch = eulerAngles(:,2);
+% roll = eulerAngles(:,3);
+% [avarYaw,~] = allanvar(yaw,'octave',Fs);
+% [avarPitch,~] = allanvar(pitch,'octave',Fs);
+% [avarRoll,tau] = allanvar(roll,'octave',Fs);
+% tbl = table(tau, avarYaw, avarPitch, avarRoll);
+% loglog(tbl, "tau", ["avarYaw" "avarPitch" "avarRoll"]);
+
 [avar,tau] = allanvar(accel_all,'octave',Fs);
 axAvar = avar(:,1);
 ayAvar = avar(:,2);
 azAvar = avar(:,3);
-
 tbl = table(tau, axAvar, ayAvar, azAvar);
-head(tbl,3)
 loglog(tbl, "tau", ["axAvar" "ayAvar" "azAvar"]);
+
 grid on;
 legend;
 xlabel('\tau');
--- imu.m
+++ imu.m
@@ -7,7 +7,7 @@ buffer_length = 400;
 fs = 200;
 f_range = (0:buffer_length-1)*(fs/buffer_length);
 fshift = (-buffer_length/2:buffer_length/2-1)*(fs/buffer_length);
-an_fft = true;
+an_mode = 3; % 1acceleration 2fft 3eular
 
 % 创建数据缓冲区
 ax_data = zeros(1,buffer_length);
@@ -24,27 +24,45 @@ imu_obj = mpu6050(ESP_obj,'OutputFormat','matrix','TimeFormat','duration','I2CAd
 disp("MPU Connected");
 
 % 定义动态绘图结构体
-if an_fft==false
-    disp("Running at normal mode");
-    an_ax = animatedline('MaximumNumPoints',2000,'Color',"#0072BD");
-    an_ay = animatedline('MaximumNumPoints',2000,'Color',"#D95319");
-    an_az = animatedline('MaximumNumPoints',2000,'Color',"#EDB120");
-    % an_gx = animatedline('MaximumNumPoints',2000,'Color',"#7E2F8E");
-    % an_gy = animatedline('MaximumNumPoints',2000,'Color',"#4DBEEE");
-    % an_gz = animatedline('MaximumNumPoints',2000,'Color',"#A2142F");
-else
-    disp("Running at fft mode");
-    an_ax_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#0072BD");
-    an_ay_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#D95319");
-    an_az_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#EDB120");
+switch an_mode
+    case 1
+        disp("Running at normal mode");
+        an_ax = animatedline('MaximumNumPoints',2000,'Color',"#0072BD");
+        an_ay = animatedline('MaximumNumPoints',2000,'Color',"#D95319");
+        an_az = animatedline('MaximumNumPoints',2000,'Color',"#EDB120");
+        % an_gx = animatedline('MaximumNumPoints',2000,'Color',"#7E2F8E");
+        % an_gy = animatedline('MaximumNumPoints',2000,'Color',"#4DBEEE");
+        % an_gz = animatedline('MaximumNumPoints',2000,'Color',"#A2142F");
+        drawnow
+        title('Acceleration');
+        xlabel("t/ms");
+        ylabel("m^2/s");
+        legend('ax','ay','az');
+    case 2
+        disp("Running at fft mode");
+        an_ax_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#0072BD");
+        an_ay_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#D95319");
+        an_az_fft = animatedline('MaximumNumPoints',length(fshift),'Color',"#EDB120");
+        drawnow
+        title('FFT');
+        xlabel("f (Hz)");
+        ylabel("|P(f)|");
+        legend('ax','ay','az');
+    case 3
+        disp("Running at eular mode");
+        an_yaw = animatedline('MaximumNumPoints',2000,'Color',"#0072BD");
+        an_pitch = animatedline('MaximumNumPoints',2000,'Color',"#D95319");
+        an_roll = animatedline('MaximumNumPoints',2000,'Color',"#EDB120");
+        drawnow
+        title('Eular Angle');
+        xlabel("t/ms");
+        ylabel("degree");
+        legend('yaw','pitch','roll');
 end
-drawnow
-xlabel("f (Hz)");
-ylabel("|P(f)|");
-legend('ax','ay','az');
 
 data_cnt = 0;
 buffer_available = false;
+fuse = complementaryFilter("SampleRate",200,"HasMagnetometer",false,"OrientationFormat","quaternion");
 while 1
     [accel, gyro, timeStamps, ~] = imu_obj.read;
 
@@ -67,26 +85,34 @@ while 1
     end
 
     %动态绘图
-    if an_fft==false
-        addpoints(an_ax,duration_ms,ax_t);
-        addpoints(an_ay,duration_ms,ay_t);
-        addpoints(an_az,duration_ms,az_t);
-%         addpoints(an_gx,duration_ms,gx_t);
-%         addpoints(an_gy,duration_ms,gy_t);
-%         addpoints(an_gz,duration_ms,gz_t);
-    else
-        if buffer_available
-            ax_fft = fftshift(fft(ax_data));
-            ay_fft = fftshift(fft(ay_data));
-            az_fft = fftshift(fft(az_data));
-            ax_power = abs(ax_fft).^2/buffer_length;
-            ay_power = abs(ay_fft).^2/buffer_length;
-            az_power = abs(az_fft).^2/buffer_length;
-            %绘图
-            addpoints(an_ax_fft,fshift,ax_power);
-            addpoints(an_ay_fft,fshift,ay_power);
-            addpoints(an_az_fft,fshift,az_power);
-        end
+    switch an_mode
+        case 1
+            addpoints(an_ax,duration_ms,ax_t);
+            addpoints(an_ay,duration_ms,ay_t);
+            addpoints(an_az,duration_ms,az_t);
+%             addpoints(an_gx,duration_ms,gx_t);
+%             addpoints(an_gy,duration_ms,gy_t);
+%             addpoints(an_gz,duration_ms,gz_t);
+        case 2
+            if buffer_available
+                ax_fft = fftshift(fft(ax_data));
+                ay_fft = fftshift(fft(ay_data));
+                az_fft = fftshift(fft(az_data));
+                ax_power = abs(ax_fft).^2/buffer_length;
+                ay_power = abs(ay_fft).^2/buffer_length;
+                az_power = abs(az_fft).^2/buffer_length;
+                %绘图
+                addpoints(an_ax_fft,fshift,ax_power);
+                addpoints(an_ay_fft,fshift,ay_power);
+                addpoints(an_az_fft,fshift,az_power);
+            end
+        case 3
+            [orientation_q,~] = fuse(accel,gyro);
+            eulerAngles = euler(orientation_q,'ZYX','point');
+            eulerAngles = rad2deg(eulerAngles);
+            addpoints(an_yaw    ,duration_ms,eulerAngles(:,1));
+            addpoints(an_pitch  ,duration_ms,eulerAngles(:,2));
+            addpoints(an_roll   ,duration_ms,eulerAngles(:,3));
     end
     drawnow
 end
