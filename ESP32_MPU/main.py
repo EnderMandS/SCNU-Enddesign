46f06b6573f11c425fe964027eaf4b9290dcb7a3
M <952025764@qq.com>
2023-03-15 00:33:27 +0800

    增加串口(bug

--- ESP32_MPU/main.py
+++ ESP32_MPU/main.py
@@ -3,23 +3,31 @@ Description:
 Autor: M
 Date: 2023-03-13 21:54:04
 LastEditors: M
-LastEditTime: 2023-03-14 19:32:27
+LastEditTime: 2023-03-15 00:33:02
 '''
 
 import machine
-from systim import SYSTIM
 from mpu import MPU6050
 
 def main():
     machine.freq(240000000)
     print("\n\nThis is the program of MPU6050")
-    print('CPU clock: %dHz\n' % machine.freq())
-    # tim = SYSTIM()
-    # tim.go_on()
+    print('CPU clock: %dHz' % machine.freq())
+
+    # scl=18, sda=19
     i2c_obj = machine.I2C(0, freq=400000)
-    # print(i2c_obj.scan())
     mpu_obj = MPU6050(i2c=i2c_obj)
-    print(mpu_obj.get_values())
+    print('MPU Init success')
+    # print(mpu_obj.get_raw_array())
+
+    # 偶校验 115200 tx=10, rx=9
+    uart = machine.UART(1,baudrate=115200, txbuf=1024, rxbuf=129, \
+                         parity=0 , timeout=100, timeout_char=100)
+    print('UART Init success')
+    
+    while 1:
+        data = mpu_obj.get_raw_values()
+        print(uart.write(data))
     
 if __name__ == '__main__':
     main()
--- ESP32_MPU/mpu.py
+++ ESP32_MPU/mpu.py
@@ -3,21 +3,26 @@ Description:
 Autor: M
 Date: 2023-03-13 21:55:43
 LastEditors: M
-LastEditTime: 2023-03-14 19:25:24
+LastEditTime: 2023-03-14 22:22:35
 '''
-import machine
+import time
 class MPU6050():
+
     def __init__(self, i2c, addr=0x68):
         self.iic = i2c
         self.addr = addr
-        # self.iic.start()
-        self.iic.writeto(self.addr, bytearray([107, 0]))
-        # self.iic.stop()
 
+        # http://www.51hei.com/bbs/dpj-136277-1.html
+        # MPU掉电后所有设置清零
+        # 使能温度
+        i2c.writeto_mem(addr, 0x6B, bytearray([0])) 
+        # 不分频，关闭滤波器，g不自检 +-2000°/s，a不自检 +-2g
+        i2c.writeto_mem(addr, 0x19, bytearray([0, 0b00000111, 0b00011111,0]))
+        # 关闭FIFO
+        i2c.writeto_mem(addr, 0x23, bytearray([0])) 
+        
     def get_raw_values(self):
-        # self.iic.start()
         a = self.iic.readfrom_mem(self.addr, 0x3B, 14)
-        # self.iic.stop()
         return a
 
     def get_ints(self):
@@ -35,12 +40,14 @@ class MPU6050():
     def get_values(self):
         raw_ints = self.get_raw_values()
         vals = {}
+        vals["time"] = time.ticks_us()
         vals["ax"] = self.bytes_toint(raw_ints[0], raw_ints[1])
         vals["ay"] = self.bytes_toint(raw_ints[2], raw_ints[3])
         vals["az"] = self.bytes_toint(raw_ints[4], raw_ints[5])
-        vals["tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) / 340.00 + 36.53
+        vals["tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) # / 340.00 + 36.53
         vals["gx"] = self.bytes_toint(raw_ints[8], raw_ints[9])
         vals["gy"] = self.bytes_toint(raw_ints[10], raw_ints[11])
         vals["gz"] = self.bytes_toint(raw_ints[12], raw_ints[13])
         return vals  # returned in range of Int16
         # -32768 to 32767
+        
