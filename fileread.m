708f189a7486780dec571172f3875db128c20c05
M <952025764@qq.com>
2023-03-21 20:44:18 +0800

    添加艾伦方差分析

--- fileread.m
+++ fileread.m
@@ -6,20 +6,23 @@ disp('Loading file');
 load( strcat('data/','2023-03-20-23-13-06','.mat') );
 
 time_len = length(time);
-fs = time_len / (time(time_len)-time(1));
+fs = floor(time_len / (time(time_len)-time(1)));
 fft_len = (-time_len/2:time_len/2-1)*(fs/time_len);
 
 for i = time_len:1  % time从0开始
     time(i) = time(i)-time(1);
 end
 
+% 加速度
 figure('Name','Accleration');
 title('Accleration');
 xlabel('t/s');
 ylabel('m^2/s');
 plot(time,accel(:,1), time,accel(:,2), time,accel(:,3));
+grid on;
 legend('ax','ay','az');
 
+% FFT
 ax_fft = fftshift(fft(accel(:,1)));
 ay_fft = fftshift(fft(accel(:,2)));
 az_fft = fftshift(fft(accel(:,3)));
@@ -31,8 +34,10 @@ title('FFT');
 xlabel("f (Hz)");
 ylabel("|P(f)|");
 plot(fft_len,ax_power, fft_len,ay_power, fft_len,az_power);
+grid on;
 legend('ax','ay','az');
 
+% 互补对称滤波Euler角
 fuse = complementaryFilter("SampleRate",fs,"HasMagnetometer",false,"OrientationFormat","quaternion");
 [orientation_q,~] = fuse(accel,gyro);
 eulerAngles = rad2deg(euler(orientation_q,'ZYX','point'));
@@ -41,4 +46,19 @@ title('Eular Angle');
 xlabel("t/s");
 ylabel("degree");
 plot(time,eulerAngles(:,1), time,eulerAngles(:,2), time,eulerAngles(:,3));
+grid on;
 legend('yaw','pitch','roll');
+
+% Allan方差分析
+[avar,tau] = allanvar(accel,1:floor((time_len-1)/2),fs);
+axAvar = avar(:,1);
+ayAvar = avar(:,2);
+azAvar = avar(:,3);
+tbl = table(tau, axAvar, ayAvar, azAvar);
+figure('Name','Allan Variance');
+loglog(tbl, "tau", ["axAvar" "ayAvar" "azAvar"]);
+grid on;
+legend;
+xlabel('\tau');
+ylabel('\sigma^2(\tau)');
+title('Allan Variance');
