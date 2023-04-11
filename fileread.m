69d2d298ac7e742a17cb1e6b9ec141f553a7cbbb
M <952025764@qq.com>
2023-04-11 23:40:38 +0800

    增加陀螺仪读取显示

--- fileread.m
+++ fileread.m
@@ -16,22 +16,60 @@ end
 % ¼ÓËÙ¶È
 disp('Calculating accleration');
 figure('Name','Accleration');
-title('Accleration');
-xlabel('t/s');
-ylabel('m^2/s');
-plot(time,accel(:,1), time,accel(:,2), time,accel(:,3));
+
+subplot(3,1,1);
+plot(time,accel(:,1));
+xlabel('t/s')
+ylabel('m^2/s')
 grid on;
-legend('ax','ay','az');
+title('ax');
+xlim([0 62]);
+
+subplot(3,1,2);
+plot(time,accel(:,2));
+xlabel('t/s')
+ylabel('m^2/s')
+grid on;
+title('ay');
+xlim([0 62]);
+
+subplot(3,1,3);
+plot(time,accel(:,3));
+xlabel('t/s')
+ylabel('m^2/s')
+grid on;
+title('az');
+xlim([0 62]);
+
 
 % ÍÓÂÝÒÇ
 disp('Calculating gyroscope');
 figure('Name','Gyro');
 title('Gyroscope');
+
+subplot(3,1,1);
+plot(time,gyro(:,1));
 xlabel('t/s');
 ylabel('rad/s');
-plot(time,gyro(:,1), time,gyro(:,2), time,gyro(:,3));
 grid on;
-legend('gx','gy','gz');
+title('gx');
+xlim([0 62]);
+
+subplot(3,1,2);
+plot(time,gyro(:,2));
+xlabel('t/s');
+ylabel('rad/s');
+grid on;
+title('gy');
+xlim([0 62]);
+
+subplot(3,1,3);
+plot(time,gyro(:,3));
+xlabel('t/s');
+ylabel('rad/s');
+grid on;
+title('gz');
+xlim([0 62]);
 
 % FFT
 disp('Calculating FFT');
@@ -41,65 +79,78 @@ az_fft = fftshift(fft(accel(:,3)));
 ax_power = abs(ax_fft).^2/time_len;
 ay_power = abs(ay_fft).^2/time_len;
 az_power = abs(az_fft).^2/time_len;
-figure('Name', 'FFT');
-title('FFT');
+figure('Name', 'FFT a');
+plot(fft_len,ax_power, fft_len,ay_power, fft_len,az_power);
 xlabel("f (Hz)");
 ylabel("|P(f)|");
-plot(fft_len,ax_power, fft_len,ay_power, fft_len,az_power);
 grid on;
 legend('ax','ay','az');
 
-% »¥²¹¶Ô³ÆÂË²¨Euler½Ç
-disp('Calculating complementary eular angle');
-
-% comFilter = complementaryFilter("SampleRate",fs,"HasMagnetometer",false, ... 
-%     "OrientationFormat","quaternion","AccelerometerGain",0.8);
-% [orientation_q,~] = comFilter(accel,gyro);
-
-addpath('quaternion_library');
-AHRS = MahonyAHRS('SamplePeriod',1/fs, 'Kp',0.5, 'Ki',0.01);
-orientation_q = zeros(time_len, 4);
-for i = 1:time_len
-    AHRS.UpdateIMU(gyro(i,:), accel(i,:)/9.7883);	% gyroscope units must be radians
-    orientation_q(i,:) = AHRS.Quaternion;
-end
-orientation_q = quaternion(quaternConj(orientation_q));
-
-eulerAngles = rad2deg(euler(orientation_q,'ZYX','point'));
-figure('Name', 'Complementary filter eular angle');
-title('Complementary filter eular angle');
-xlabel("t/s");
-ylabel("degree");
-plot(time,eulerAngles(:,1), time,eulerAngles(:,2), time,eulerAngles(:,3));
+gx_fft = fftshift(fft(gyro(:,1)));
+gy_fft = fftshift(fft(gyro(:,2)));
+gz_fft = fftshift(fft(gyro(:,3)));
+gx_power = abs(gx_fft).^2/time_len;
+gy_power = abs(gy_fft).^2/time_len;
+gz_power = abs(gz_fft).^2/time_len;
+figure('Name', 'FFT g');
+plot(fft_len,gx_power, fft_len,gy_power, fft_len,gz_power);
+xlabel("f (Hz)");
+ylabel("|P(f)|");
 grid on;
-legend('yaw','pitch','roll');
+legend('gx','gy','gz');
 
-% EKF
-disp('Calculating EKF eular angle');
-clear eulerAngles;
-clear orientation_q;
-ins_accel = insAccelerometer;
-ins_gyro = insGyroscope;
-filter_EKF = insEKF(ins_accel,ins_gyro);
-stateparts(filter_EKF,"Orientation",[1 0 0 0]);
-statecovparts(filter_EKF,"Orientation",1e-2);
-accNoise = 0.01;
-gyroNoise = 0.01;
-processNoise = diag([0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1]);
-filter_EKF.AdditiveProcessNoise = processNoise;
-for i = 1:length(time)
-    fuse(filter_EKF,ins_accel,accel(i,:),diag([0.1 0.1 0.1]));
-    fuse(filter_EKF,ins_gyro,gyro(i,:),diag([0.2 0.2 0.2]));
-    orientation_q(i) = quaternion(stateparts(filter_EKF,"Orientation"));
-end
-eulerAngles = rad2deg(euler(orientation_q,'ZYX','point'));
-figure('Name', 'EKF eular angle');
-title('EKF eular angle');
-xlabel("t/s");
-ylabel("degree");
-plot(time,eulerAngles(:,1), time,eulerAngles(:,2), time,eulerAngles(:,3));
-grid on;
-legend('yaw','pitch','roll');
+% 
+% % »¥²¹¶Ô³ÆÂË²¨Euler½Ç
+% disp('Calculating complementary eular angle');
+% 
+% % comFilter = complementaryFilter("SampleRate",fs,"HasMagnetometer",false, ... 
+% %     "OrientationFormat","quaternion","AccelerometerGain",0.8);
+% % [orientation_q,~] = comFilter(accel,gyro);
+% 
+% addpath('quaternion_library');
+% AHRS = MahonyAHRS('SamplePeriod',1/fs, 'Kp',0.5, 'Ki',0.01);
+% orientation_q = zeros(time_len, 4);
+% for i = 1:time_len
+%     AHRS.UpdateIMU(gyro(i,:), accel(i,:)/9.7883);	% gyroscope units must be radians
+%     orientation_q(i,:) = AHRS.Quaternion;
+% end
+% orientation_q = quaternion(quaternConj(orientation_q));
+% 
+% eulerAngles = rad2deg(euler(orientation_q,'ZYX','point'));
+% figure('Name', 'Complementary filter eular angle');
+% title('Complementary filter eular angle');
+% xlabel("t/s");
+% ylabel("degree");
+% plot(time,eulerAngles(:,1), time,eulerAngles(:,2), time,eulerAngles(:,3));
+% grid on;
+% legend('yaw','pitch','roll');
+% 
+% % EKF
+% disp('Calculating EKF eular angle');
+% clear eulerAngles;
+% clear orientation_q;
+% ins_accel = insAccelerometer;
+% ins_gyro = insGyroscope;
+% filter_EKF = insEKF(ins_accel,ins_gyro);
+% stateparts(filter_EKF,"Orientation",[1 0 0 0]);
+% statecovparts(filter_EKF,"Orientation",1e-2);
+% accNoise = 0.01;
+% gyroNoise = 0.01;
+% processNoise = diag([0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1]);
+% filter_EKF.AdditiveProcessNoise = processNoise;
+% for i = 1:length(time)
+%     fuse(filter_EKF,ins_accel,accel(i,:),diag([0.1 0.1 0.1]));
+%     fuse(filter_EKF,ins_gyro,gyro(i,:),diag([0.2 0.2 0.2]));
+%     orientation_q(i) = quaternion(stateparts(filter_EKF,"Orientation"));
+% end
+% eulerAngles = rad2deg(euler(orientation_q,'ZYX','point'));
+% figure('Name', 'EKF eular angle');
+% title('EKF eular angle');
+% xlabel("t/s");
+% ylabel("degree");
+% plot(time,eulerAngles(:,1), time,eulerAngles(:,2), time,eulerAngles(:,3));
+% grid on;
+% legend('yaw','pitch','roll');
 
 % Allan·½²î·ÖÎö
 disp('Calculating allanvar');
@@ -108,10 +159,36 @@ axAvar = avar(:,1);
 ayAvar = avar(:,2);
 azAvar = avar(:,3);
 tbl = table(tau, axAvar, ayAvar, azAvar);
-figure('Name','Allan Variance');
+figure('Name','Allan Variance a');
 loglog(tbl, "tau", ["axAvar" "ayAvar" "azAvar"]);
+% xline(sqrt(2));
+% xline(sqrt(3));
+% xline(3);
+% xline(1);
 grid on;
 legend;
-xlabel('\tau');
-ylabel('\sigma^2(\tau)');
-title('Allan Variance');
+xlabel('T');
+ylabel('\sigma(T)');
+text(1,1,'ax');
+text(1,2,'ay');
+text(1,4,'az');
+
+[avar,tau] = allanvar(gyro,1:floor((time_len-1)/2),fs);
+gxAvar = avar(:,1);
+gyAvar = avar(:,2);
+gzAvar = avar(:,3);
+tbl = table(tau, gxAvar, gyAvar, gzAvar);
+figure('Name','Allan Variance g');
+loglog(tbl, "tau", ["gxAvar" "gyAvar" "gzAvar"]);
+% xline(sqrt(2));
+% xline(sqrt(3));
+% xline(3);
+% xline(1);
+grid on;
+legend;
+xlabel('T');
+ylabel('\sigma(T)');
+text(1,0.01,'gx');
+text(1,0.02,'gy');
+text(1,0.1,'gz');
+
